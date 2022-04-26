2ec64e1e69ac82517f39950d156730ae95ab62d6
raulmc8 <raulmc8@gmail.com>
2022-04-26 18:00:25 -0500

    se corrigen errores

--- components/hero.jsx
+++ components/hero.jsx
@@ -12,7 +12,7 @@ const Hero = () =>{
             industry, constantly updating and practicing hard to achive it</p>
             <div className="flex justify-center">
             <a href="../public/Raúl Márquez Castillo-3.pdf" download="Marquez, Raul - CV.pdf">
-            <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Download CV</button></a>
+            <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Download CV</button></a>
         
         </div>
         </div>
